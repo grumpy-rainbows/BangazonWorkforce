094fc32b7763ada892830d3c540bce4eaa8d603d
Jameka Echols <jameka.echols@gmail.com>
2019-07-09 16:21:22 -0500

    able to create a computer and get the purchase date set to the right time

--- BangazonWorkforce/BangazonWorkforce/Models/ViewModels/ComputerCreateViewModel.cs
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/ComputerCreateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,6 @@ namespace BangazonWorkforce.Models.ViewModels
 {
     public class ComputerCreateViewModel
     {
+        public Computer Computer { get; set; }
     }
 }
