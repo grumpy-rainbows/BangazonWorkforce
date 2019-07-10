2ff3a06d642f04a3b458f7b52688ab143cc568da
selam <selam0315@gmail.com>
2019-07-10 15:08:27 -0500

    create employee view model for detail

--- BangazonWorkforce/BangazonWorkforce/Models/ViewModels/EmployeeDetailViewModel.cs
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/EmployeeDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace BangazonWorkforce.Models.ViewModels
 {
     public class EmployeeDetailViewModel
     {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+
+        public Computer Computer { get; set; }
+
+        public List<TrainingProgram> TrainingProgramList { get; set; }
     }
 }
